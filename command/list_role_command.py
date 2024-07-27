d7e85a93d40095b4fa684284cf9e94fc0fbebfe7
Merzouz <geoffroymerlin@outlook.fr>
2024-07-28 01:54:53 +0200

    Update list_role_command.py

--- command/list_role_command.py
+++ command/list_role_command.py
@@ -6,22 +6,18 @@ from database.role_database import list_roles
 async def list_roles_command(interaction: discord.Interaction):
     print(f"Interaction: {interaction}")
     
-    # Récupérer les rôles de la base de données
     roles = list_roles()
     
     if roles:
-        # Créer un embed pour afficher les rôles
         embed = discord.Embed(
             title="List of Roles",
             description="Here are all the roles in the database:",
             color=discord.Color.green()
         )
         
-        # Ajouter chaque rôle à l'embed
         for role in roles:
             embed.add_field(name="Role", value=role, inline=False)
         
-        # Envoyer l'embed
         await interaction.response.send_message(embed=embed)
     else:
         await interaction.response.send_message("No roles found in the database.", ephemeral=True)
